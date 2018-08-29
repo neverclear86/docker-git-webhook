c7c1344abb7eef89ebfa82741d24ecdc33b6491b
ss-ai-p001 <a>
2018-08-29 11:28:52 +0900

    とりあえず動くやで

--- app/index.js
+++ app/index.js
@@ -1,14 +1,20 @@
 const http = require('http')
 const childProcess = require('child_process')
+const fs = require('fs')
 
 const GIT_DIR = '/code'
-const GIT_REPO = process.env.GET_REPO
+const GIT_REPO = process.env.GIT_REPO
 
-let dir = childProcess.execSync(`ls -a ${GIT_DIR}`)
+fs.mkdir(GIT_DIR, (err) => {if(err) console.log(err)})
+let dir = childProcess.execSync(`ls -a1 ${GIT_DIR} | wc -l`).toString()
+if (Number(dir) <= 2) {
+  // clone
+  childProcess.execSync(`git clone ${GIT_REPO} /code`)
+}
 
-
-function gitPull(dirname) {
-  childProcess.exec(`cd ${dirname} && git pull`)
+function gitPull(dirName) {
+  childProcess.exec(`cd ${dirName} && git pull`)
+  console.log('pulled ' + GIT_REPO)
 }
 
 http.createServer((req, res) => {
